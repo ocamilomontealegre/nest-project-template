4e6463cf8f98532e17a864a5353d0d4a019254ea
ocamilomontealegre <ocamilomontealegre@gmail.com>
2024-09-14 15:09:30 -0500

    chore: set OpenAPI for health module

--- src/health/controllers/health.controller.ts
+++ src/health/controllers/health.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Inject } from "@nestjs/common";
 import { HEALTH_ENDPOINT, HEALTH_SERVICE } from "../constants";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import type { IHealthMessage } from "../interfaces/health-message.interface";
 
+@ApiTags("Health")
 @Controller(HEALTH_ENDPOINT)
 export class HealthController {
   public constructor(@Inject(HEALTH_SERVICE) private readonly _healthService: IHealthMessage) {}
 
   @Get()
+  @ApiOperation({ summary: "Checks app status" })
+  @ApiResponse({ status: 200, description: "Service up and running" })
   public check(): IHealthMessage {
     return this._healthService;
   }
